a52f05a8d2ba09de71ca27be22609e01127ba433
Luís Vieira <48384492+30Massas@users.noreply.github.com>
2020-01-22 16:03:14 +0000

    Update q19.hs

--- q19.hs
+++ q19.hs
@@ -2,3 +2,8 @@
 myElemIndices :: Eq a => a -> [a] -> [Int]
 myElemIndices _ [] = []
 myElemIndices x (h:t) = aux1 0 x (h:t)
+
+aux1 :: Eq a => Int -> a -> [a] -> [Int]
+aux1 _ _ [] = []
+aux1 x y (h:t) | y == h = x:aux1 (x+1) y t
+               | otherwise = aux1 (x+1) y t
